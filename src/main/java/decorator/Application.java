6149260982e0ede4def5efa1d407462cc1309144
Ravil Galeyev <dehasi@yandex.ru>
2024-02-29 22:30:48 +0100

    x

---
+++ src/main/java/decorator/Application.java
@@ -0,0 +1,14 @@
+package decorator;
+
+import java.io.*;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        LineNumberReader reader =
+                new LineNumberReader(new BufferedReader(new FileReader("README.adoc")));
+
+        System.out.println(reader.getLineNumber());
+        System.out.println(reader.readLine());
+    }
+}
