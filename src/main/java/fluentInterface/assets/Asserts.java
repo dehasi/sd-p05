9aa112eedc828d34dba6028cba1cff2c01a5087b
Ravil Galeyev <dehasi@yandex.ru>
2024-03-04 22:17:32 +0100

    x

--- src/main/java/fluentInterface/assets/Asserts.java
+++ src/main/java/fluentInterface/assets/Asserts.java
@@ -1,9 +1,21 @@
 package fluentInterface.assets;
 
+import java.util.List;
+
 class Asserts {
     public static void main(String[] args) {
 
+        Object o = null;
+        String s = "32";
+
+        if (Check.that(o).isNotNull()) {
+            //do smth
+        }
+
+        Check.that(List.of())
+                .hasSize(42)
+                .onConditionFail(() -> {
+                    throw new RuntimeException("HEre");
+                });
     }
-
-
 }
