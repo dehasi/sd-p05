734275876b7a6040c408aff7a84d535a98db811a
Ravil Galeyev <dehasi@yandex.ru>
2024-03-05 22:23:38 +0100

    x

---
+++ src/main/js/chaning.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const fetchPromise = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+);
+
+fetchPromise
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data[0].name);
+  });
